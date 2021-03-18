fd26dcc1d0c2668b64829c159e394d26ce09a1ee
Jonathan Falvey <55098346+JDirac@users.noreply.github.com>
2021-03-18 22:23:19 +0000

    Update Player.h

--- Player.h
+++ Player.h
@@ -5,19 +5,28 @@
 class Player: public Entity {
     private:
         vector<Item> inv; // Player inventory
-        Item equippedWeapon;
+        Item *equippedWeapon = nullptr;
+        Item *equippedArmor = nullptr;
         int wealth;
         State status; // In combat? No? Ok.
     public:
-        void equipItem();
-        string showInventory();
-        /*
-         *  Special skills?
-         */
+        Player(string name, string description, float HP, float ATK, float DEF, float ACC, float CRT, int wealth);
+        void takeItem(Item& target) { inv.push_back(target);};
+        Item* putItem(string itemName);
+        void showInventory();
+        Item getWeapon() { return *equippedWeapon; }; // returns equipped weapon.
+        Item getArmor() { return *equippedArmor; }; // returns equipped armor.
+        void setEquippedWeapon(Item item) { equippedWeapon = &item; };
+        void setEquippedArmor(Item item) { equippedArmor = &item; };
+        int getWealth() { return wealth;}; // returns player's wealth
+        void setWealth(int wealth) { this->wealth = wealth; };
+        void equipItem(Item target);
+        void equipItem(string name);
         void defend(); // combat
         void escape(); // Non boss combat
-        Item getWeapon(); // returns equipped weapon.
-        int getWealth(); // returns player's wealth
+        /*
+        *  Special skills?
+        */
 
 };
 
