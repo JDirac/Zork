fb1e3b52002c1fb8401342ba3a8bcc66a0869b11
Jonathan Falvey <55098346+JDirac@users.noreply.github.com>
2021-03-18 22:24:44 +0000

    Update Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,3 +1,12 @@
 #include "Entity.h"
 
 //Enter Methods for Entity
+Entity::Entity(string name, string description, float HP, float ATK, float DEF, float ACC, float CRT) {
+    this->name = name;
+    this->description = description;
+    this->HP = HP;
+    this->ATK = ATK;
+    this->DEF = DEF;
+    this->ACC = ACC;
+    this->CRT = CRT;
+}
