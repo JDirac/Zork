21a41b53d09bc26d5aacfc1221f39cb8b37356fd
Jonathan Falvey <55098346+JDirac@users.noreply.github.com>
2021-04-10 17:42:27 +0100

    Initialiser List

--- Entity.cpp
+++ Entity.cpp
@@ -1,12 +1,5 @@
 #include "Entity.h"
 
 //Enter Methods for Entity
-Entity::Entity(string name, string description, int HP, int ATK, int DEF, float ACC, float CRT) {
-    this->name = name;
-    this->description = description;
-    this->HP = HP;
-    this->ATK = ATK;
-    this->DEF = DEF;
-    this->ACC = ACC;
-    this->CRT = CRT;
-}
+Entity::Entity(string name, string description, int HP, int ATK, int DEF, float ACC, float CRT)
+    :name(name), description(description), HP(HP), ATK(ATK), DEF(DEF), ACC(ACC), CRT(CRT) { }
